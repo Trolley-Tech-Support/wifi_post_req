d4b1adb5b445d84dac4444a6f8bdcd01937d5c4e
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-03-19 18:37:25 +0000

    add esp sample setup

--- main/station_example_main.c
+++ main/station_example_main.c
@@ -19,25 +19,34 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include <esp_http_client.h>
-#include <cJSON.h> 
+#include <cJSON.h>
 #define MAX_HTTP_RECV_BUFFER 512
 #define MAX_HTTP_OUTPUT_BUFFER 2048
 
-const char* TARGET_URL = "http://3.249.30.30:1037/item/details";
+const char *TARGET_URL = "http://3.249.30.30:1037/item/details";
 const int TARGET_PORT = 80; // Assuming HTTP, adjust for HTTPS
-const char* JSON_BODY = "{\"id\": \"1\", \"name\": \"Product A\"}";
-static const char* TAG = "POST_REQUEST";
+const char *JSON_BODY = "{\"id\": \"1\", \"name\": \"Product A\"}";
+// static const char *TAG = "POST_REQUEST";
 char response_buffer[1024]; // Adjust size as needed for expected response
+#include <string.h>
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_netif.h"
+#include "esp_tls.h"
 
+#include "mqtt_client.h"
+
+static const char *TAG = "mesh_mqtt";
+static esp_mqtt_client_handle_t s_client = NULL;
 
 /* The examples use WiFi configuration that you can set via project configuration menu
 
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      "VM6193248"
-#define EXAMPLE_ESP_WIFI_PASS      "bpvoj9gvuuTyfmzv"
-#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+#define EXAMPLE_ESP_WIFI_SSID "VM6193248"
+#define EXAMPLE_ESP_WIFI_PASS "bpvoj9gvuuTyfmzv"
+#define EXAMPLE_ESP_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
 
 #if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
@@ -74,27 +83,34 @@ static EventGroupHandle_t s_wifi_event_group;
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define WIFI_FAIL_BIT BIT1
 
 static int s_retry_num = 0;
 
-
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
             esp_wifi_connect();
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
+        }
+        else
+        {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
@@ -140,48 +156,59 @@ void wifi_init_sta(void)
             .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
         },
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
      * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
 
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
+    if (bits & WIFI_CONNECTED_BIT)
+    {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    } else if (bits & WIFI_FAIL_BIT) {
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 }
 
-void interpret_json_response(char *response) {
+void interpret_json_response(char *response)
+{
     cJSON *root = cJSON_Parse(response);
-    if (root) {
+    if (root)
+    {
         // Get item name and price using cJSON functions
         cJSON *item_name = cJSON_GetObjectItem(root, "item");
-        if (item_name && item_name->type == cJSON_String) {
+        if (item_name && item_name->type == cJSON_String)
+        {
             ESP_LOGI(TAG, "Item name: %s", item_name->valuestring);
         }
         cJSON *price = cJSON_GetObjectItem(root, "price");
-        if (price && price->type == cJSON_String) {
+        if (price && price->type == cJSON_String)
+        {
             ESP_LOGI(TAG, "Price: Eur%s", price->valuestring);
         }
         // Free the allocated memory
         cJSON_Delete(root);
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG, "Failed to parse JSON response!");
     }
 }
@@ -190,7 +217,7 @@ static void http_native_request(void)
 {
     // Declare local_response_buffer with size (MAX_HTTP_OUTPUT_BUFFER + 1) to prevent out of bound access when
     // it is used by functions like strlen(). The buffer should only be used upto size MAX_HTTP_OUTPUT_BUFFER
-    char output_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};   // Buffer to store response of http request
+    char output_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0}; // Buffer to store response of http request
     int content_length = 0;
     esp_http_client_config_t config = {
         .url = "http://3.249.30.30:1037/item/details",
@@ -203,25 +230,35 @@ static void http_native_request(void)
     esp_http_client_set_method(client, HTTP_METHOD_POST);
     esp_http_client_set_header(client, "Content-Type", "application/json");
     esp_err_t err = esp_http_client_open(client, strlen(post_data));
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to open HTTP connection: %s", esp_err_to_name(err));
-    } else {
+    }
+    else
+    {
         int wlen = esp_http_client_write(client, post_data, strlen(post_data));
-        if (wlen < 0) {
+        if (wlen < 0)
+        {
             ESP_LOGE(TAG, "Write failed");
         }
         content_length = esp_http_client_fetch_headers(client);
-        if (content_length < 0) {
+        if (content_length < 0)
+        {
             ESP_LOGE(TAG, "HTTP client fetch headers failed");
-        } else {
+        }
+        else
+        {
             int data_read = esp_http_client_read_response(client, output_buffer, MAX_HTTP_OUTPUT_BUFFER);
-            if (data_read >= 0) {
-                ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %"PRId64,
-                esp_http_client_get_status_code(client),
-                esp_http_client_get_content_length(client));
+            if (data_read >= 0)
+            {
+                ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %" PRId64,
+                         esp_http_client_get_status_code(client),
+                         esp_http_client_get_content_length(client));
                 interpret_json_response(output_buffer);
                 // ESP_LOG_BUFFER_HEX(TAG, output_buffer, strlen(output_buffer));
-            } else {
+            }
+            else
+            {
                 ESP_LOGE(TAG, "Failed to read response");
             }
         }
@@ -229,18 +266,91 @@ static void http_native_request(void)
     esp_http_client_cleanup(client);
 }
 
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
+    switch (event->event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        if (esp_mqtt_client_subscribe(s_client, "/topic/ip_mesh/key_pressed", 0) < 0)
+        {
+            // Disconnect to retry the subscribe after auto-reconnect timeout
+            esp_mqtt_client_disconnect(s_client);
+        }
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        ESP_LOGI(TAG, "TOPIC=%.*s", event->topic_len, event->topic);
+        ESP_LOGI(TAG, "DATA=%.*s", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+    return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRId32 "", base, event_id);
+    mqtt_event_handler_cb(event_data);
+}
+
+void mqtt_app_publish(char *topic, char *publish_string)
+{
+    if (s_client)
+    {
+        int msg_id = esp_mqtt_client_publish(s_client, topic, publish_string, 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish returned msg_id=%d", msg_id);
+    }
+}
+
+void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = "mqtt://3170c27ac08647d8b9555a53da98947c.s1.eu.hivemq.cloud:8883",
+        .credentials = {
+            .username = "smart-trolley",
+            .authentication.password = "SmartTrolley123"}};
+
+    s_client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(s_client, 1234, mqtt_event_handler, s_client);
+    esp_mqtt_client_start(s_client);
+
+    char p = 'p';
+    char s = '1';
+    mqtt_app_publish(&p, &s);
+}
+
 void app_main(void)
 {
-    //Initialize NVS
+    // Initialize NVS
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
-    http_native_request();
-
+    // http_native_request();
+    mqtt_app_start();
 }
